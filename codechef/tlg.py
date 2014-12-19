762f9ba91ee5fc038112543dd177deb0d0349678
Benjamin Donovan <btdonovan@gmail.com>
2014-12-19 16:34:13 -0700

    Added code for The Lead Game. passes local testing. Submissions at codechef down at this time. Will submit later.

---
+++ codechef/tlg.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+n = int(input())
+scores = []
+lead = []
+for i in range(n):
+    scores.append(input().split())
+    for j in range(len(scores[i])):
+        scores[i][j] = int(scores[i][j])
+    if scores[i][0] > scores[i][1]:
+        scores[i].append(1)
+        lead.append(scores[i][0] - scores[i][1])
+    elif scores[i][1] > scores[i][0]:
+        scores[i].append(2)
+        lead.append(scores[i][1] - scores[i][0])
+print(scores[lead.index(max(lead))][2], max(lead))
